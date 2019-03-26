82b69ed299a77fe0aa70f48c39b11a457c09b46e
kristiwalker <kristiwalker22@gmail.com>
2019-03-26 09:56:44 -0400

    adds prop-types

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 6 */
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
